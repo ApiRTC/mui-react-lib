44658c625dc0a574fbd900fe4a73707161202db9
kmoyse <kevin.moyse@apizee.com>
2022-11-29 14:50:52 +0100

    implement setLogLevel

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 export * from './components'
 
 type LogLevel = {
+    level: 'debug' | 'info' | 'warn' | 'error'
     isDebugEnabled: boolean
     isInfoEnabled: boolean
     isWarnEnabled: boolean
 }
+
+const INFO: LogLevel = { level: 'info', isDebugEnabled: false, isInfoEnabled: true, isWarnEnabled: true };
 
 declare global {
     var apirtcMuiReactLibLogLevel: LogLevel;
 }
 
 // a default value MUST be set in case application using the library does not override it
-globalThis.apirtcMuiReactLibLogLevel = { isDebugEnabled: false, isInfoEnabled: true, isWarnEnabled: true };
+globalThis.apirtcMuiReactLibLogLevel = INFO;
+
+export function setLogLevel(logLevelText: 'debug' | 'info' | 'warn' | 'error' | string) {
+    switch (logLevelText) {
+        case 'debug':
+            globalThis.apirtcMuiReactLibLogLevel = { level: 'debug', isDebugEnabled: true, isInfoEnabled: true, isWarnEnabled: true };
+            break
+        case 'info':
+            globalThis.apirtcMuiReactLibLogLevel = INFO;
+            break
+        case 'warn':
+            globalThis.apirtcMuiReactLibLogLevel = { level: 'warn', isDebugEnabled: false, isInfoEnabled: false, isWarnEnabled: true };
+            break
+        case 'error':
+            globalThis.apirtcMuiReactLibLogLevel = { level: 'error', isDebugEnabled: false, isInfoEnabled: false, isWarnEnabled: false };
+            break
+        default:
+            // in case null is passed as input, default to 'info'
+            globalThis.apirtcMuiReactLibLogLevel = INFO;
+    }
+}
