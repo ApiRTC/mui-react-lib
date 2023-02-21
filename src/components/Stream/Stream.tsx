aca068489d96868d26a6f46d59047468700265b9
kmoyse <kevin.moyse@apizee.com>
2023-02-21 15:04:46 +0100

    Stream: default height and width to 'fit-content'

--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -20,7 +20,8 @@ const speakingBorder = {
 export type StreamProps = {
     id?: string,
     name?: string,
-    stream: ApiRtcStream,
+    nameColor?: "default" | "primary" | "secondary" | "error" | "info" | "success" | "warning" | undefined,
+    stream: ApiRtcStream | undefined,
     autoPlay?: boolean,
     muted?: boolean,
     sinkId?: string,
@@ -39,7 +40,7 @@ export default function Stream(props: StreamProps) {
     }
 
     // default autoPlay
-    const { autoPlay = true, detectSpeaking = false } = props;
+    const { autoPlay = true, detectSpeaking = false, nameColor = "primary" } = props;
 
     const { status: muted, toggleStatus: toggleMuted } = useToggle(props.muted || false);
 
@@ -82,16 +83,19 @@ export default function Stream(props: StreamProps) {
         // so that this component would register event listener only if detection is required
         //
         if (props.stream && detectSpeaking) {
+
+            const l_stream = props.stream;
+
             const on_audioAmplitude = (amplitudeInfo: any) => {
                 // { "amplitude": 102.36, "isSpeaking": true }
                 setSpeaking(amplitudeInfo.isSpeaking)
             };
-            props.stream.on('audioAmplitude', on_audioAmplitude)
+            l_stream.on('audioAmplitude', on_audioAmplitude)
             // TODO: call method to enable speaker detection
             //props.stream.enable
             return () => {
                 // TODO: call method to disable speaker detection
-                props.stream.removeListener('audioAmplitude', on_audioAmplitude)
+                l_stream.removeListener('audioAmplitude', on_audioAmplitude)
             }
         }
     }, [props.stream, detectSpeaking])
@@ -111,39 +115,46 @@ export default function Stream(props: StreamProps) {
         }
     }, [props.sinkId])
 
-    return <Box id={props.id}
-        sx={{
-            ...props.sx,
-            position: 'relative',
-            ...isSpeaking && speakingBorder
-        }}>
-        {/* {props.stream.hasVideo() ? */}
-        <video id={props.stream.getId()}
-            style={{ maxWidth: '100%', ...props.videoStyle }}
-            ref={videoRef}
-            autoPlay={autoPlay} muted={muted}
-            onMouseMove={props.onMouseMove}></video>
-        {/* <audio id={props.stream.getId()} controls
+    return <>
+        {props.stream &&
+            <Box id={props.id}
+                sx={{
+                    width: 'fit-content',
+                    height: 'fit-content',
+                    ...props.sx,
+                    position: 'relative',
+                    ...isSpeaking && speakingBorder
+                }}
+            >
+                {/* {props.stream.hasVideo() ? */}
+                <video id={props.stream.getId()}
+                    style={{ maxWidth: '100%', display: 'block', ...props.videoStyle }}
+                    ref={videoRef}
+                    autoPlay={autoPlay} muted={muted}
+                    onMouseMove={props.onMouseMove}></video>
+                {/* <audio id={props.stream.getId()} controls
                 ref={audioRef}
                 autoPlay={autoPlay} muted={muted}>
             </audio>  */}
-        {/* : <span>{props.stream.getId()}</span>)} */}
-        {props.name && <Chip sx={{
-            position: 'absolute',
-            top: 4, left: '50%', transform: 'translate(-50%)', // 4px from top and centered horizontally
-            opacity: [0.9, 0.8, 0.7],
-            zIndex: 1
-        }} label={props.name} color="primary" />}
-        <Stack sx={{
-            position: 'absolute',
-            float: 'right',
-            bottom: 4, right: 4,
-            opacity: [0.9, 0.8, 0.7],
-            zIndex: 1
-        }}>
-            <StreamContext.Provider value={{ stream: props.stream, muted, toggleMuted }}>
-                {props.controls}
-            </StreamContext.Provider>
-        </Stack>
-    </Box>
+                {/* : <span>{props.stream.getId()}</span>)} */}
+                {props.name && <Chip sx={{
+                    position: 'absolute',
+                    top: 4, left: '50%', transform: 'translate(-50%)', // 4px from top and centered horizontally
+                    opacity: [0.9, 0.8, 0.7],
+                    zIndex: 1
+                }} label={props.name} color={nameColor} />}
+                <Stack sx={{
+                    position: 'absolute',
+                    float: 'right',
+                    bottom: 4, right: 4,
+                    opacity: [0.9, 0.8, 0.7],
+                    zIndex: 1
+                }}>
+                    <StreamContext.Provider value={{ stream: props.stream, muted, toggleMuted }}>
+                        {props.controls}
+                    </StreamContext.Provider>
+                </Stack>
+            </Box>
+        }
+    </>
 }
