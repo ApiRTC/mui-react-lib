cfea0bfec02ac929d2e50ecf6a524f920def7aae
kmoyse <kevin.moyse@apizee.com>
2023-03-16 17:42:41 +0100

    Video component: manage video flow status muted to display spinner when muted

--- src/components/Stream/Video.tsx
+++ src/components/Stream/Video.tsx
@@ -1,8 +1,10 @@
-import React, { useContext, useEffect, useMemo, useRef } from 'react';
+import React, { useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 
-import { Stream } from '@apirtc/apirtc';
+import { MediaStreamTrackFlowStatus, Stream } from '@apirtc/apirtc';
 
 import { StreamContext } from './StreamContext';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export type VideoProps = {
     id?: string,
@@ -36,18 +38,37 @@ export function Video(props: VideoProps) {
 
     const videoRef = useRef<HTMLVideoElement>(null);
 
+    const [videoMuted, setVideoMuted] = useState(false);
+
     useEffect(() => {
+
         const htmlMediaElement = videoRef?.current as any;
         if (stream && htmlMediaElement) {
             if (globalThis.apirtcMuiReactLibLogLevel.isDebugEnabled) {
                 console.debug(COMPONENT_NAME + "|useEffect stream", htmlMediaElement, stream)
             }
             stream.attachToElement(htmlMediaElement)
+
+            // manage isVideoMuted to display a spinner when track is muted for technical reasons
+            // only works on chrome
+            //
+            const onVideoFlowStatusChanged = (mediaStreamTrackFlowStatus: MediaStreamTrackFlowStatus) => {
+                if (globalThis.apirtcMuiReactLibLogLevel.isDebugEnabled) {
+                    console.debug(COMPONENT_NAME + "|onVideoFlowStatusChanged", stream, mediaStreamTrackFlowStatus)
+                }
+                setVideoMuted(mediaStreamTrackFlowStatus.muted)
+            };
+            stream.on('videoFlowStatusChanged', onVideoFlowStatusChanged)
+            stream.on('remoteVideoFlowStatusChanged', onVideoFlowStatusChanged)
+
             return () => {
                 if (globalThis.apirtcMuiReactLibLogLevel.isDebugEnabled) {
                     console.debug(COMPONENT_NAME + "|useEffect stream destroy")
                 }
                 htmlMediaElement.src = "";
+
+                stream.removeListener('audioFlowStatusChanged', onVideoFlowStatusChanged)
+                stream.removeListener('remoteAudioFlowStatusChanged', onVideoFlowStatusChanged)
             }
         }
     }, [stream])
@@ -67,9 +88,13 @@ export function Video(props: VideoProps) {
         }
     }, [props.sinkId])
 
-    return <video id={id}
-        style={{ maxWidth: '100%', display: 'block', ...props.style }}
-        ref={videoRef}
-        autoPlay={autoPlay} muted={muted}
-        onMouseMove={props.onMouseMove} />
+    return <>
+        {videoMuted ? <Box sx={{ height: 200, width: 200 }}
+            display="flex" alignItems="center" justifyContent="center"><CircularProgress /></Box> :
+            <video id={id}
+                style={{ maxWidth: '100%', display: 'block', ...props.style }}
+                ref={videoRef}
+                autoPlay={autoPlay} muted={muted}
+                onMouseMove={props.onMouseMove} />}
+    </>
 }
