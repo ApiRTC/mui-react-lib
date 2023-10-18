bc1d7d34cb650cad588c936a8254746d5e7e948e
kmoyse <kevin.moyse@apizee.com>
2023-10-18 11:48:16 +0200

    1.0.10: MediaDeviceSelect: only add default device if browser does not already add one.

--- src/components/MediaDeviceSelect/MediaDeviceSelect.spec.tsx
+++ src/components/MediaDeviceSelect/MediaDeviceSelect.spec.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor, within } from "@testing-library/react";
+import UserEvent from '@testing-library/user-event';
+
 import { unmountComponentAtNode } from "react-dom";
 
 import { setLogLevel } from '../..';
-import { MediaDeviceSelect } from "./MediaDeviceSelect";
+
+import { MediaDeviceSelect, MediaDeviceSelectProps } from "./MediaDeviceSelect";
+
+import '../../mock/getDisplayMedia.mock';
+
+import { MediaDevice } from "@apirtc/apirtc";
 
 // Set log level to max to maximize code coverage
 setLogLevel('debug')
+
+jest.mock('@apirtc/apirtc', () => {
+  const originalModule = jest.requireActual('@apirtc/apirtc');
+  return {
+    __esModule: true,
+    ...originalModule,
+    MediaDevice: jest.fn().mockImplementation((id: string, type: string, label: string) => {
+      return {
+        getId: () => { return id },
+        getType: () => type,
+        getLabel: () => label
+      }
+    }),
+  }
+})
 
 let container: any = null;
 beforeEach(() => {
@@ -24,14 +46,78 @@ afterEach(() => {
 
 it("renders", () => {
 
-  const props = {
+  // First, test that a 'default' is generated if the browser does not provide some
+  const props: MediaDeviceSelectProps = {
     devices: {},
     selectedDevice: undefined,
     setSelectedDevice: jest.fn()
   };
   const { rerender } = render(<MediaDeviceSelect {...props} />);
 
-  // const audioVideo = screen.getByRole("select", { name: "Audio & Video" });
-  // expect((audioVideo as any).checked).toBe(true);
+  const select = screen.getByTestId('test-media-device-selector');
+  const btn = screen.getByRole("button");
 
+  expect(select.textContent).toContain('Default');
+  expect(btn.textContent).toBe('Default');
+  expect(props.selectedDevice).toBeUndefined();
+
+  // With a default already set by browser (chrome does this, firefox does not)
+  const browserDefault = new MediaDevice('default', 'audioinput', 'BDefault');
+  props.devices = {
+    'default': browserDefault,
+  };
+  rerender(<MediaDeviceSelect {...props} />);
+
+  expect(btn.textContent).toBe(browserDefault.getLabel());
+  expect(props.selectedDevice).toBeUndefined();
+
+  // With another device and a selected one
+  const deviceA1 = new MediaDevice('a1', 'audioinput', 'deviceA1');
+  props.devices = {
+    [browserDefault.getId()]: browserDefault,
+    [deviceA1.getId()]: deviceA1
+  };
+  rerender(<MediaDeviceSelect {...props} />);
+
+  expect(btn.textContent).toBe(browserDefault.getLabel());
+  expect(props.selectedDevice).toBeUndefined();
+
+  // With a selected device
+  props.selectedDevice = deviceA1;
+  rerender(<MediaDeviceSelect {...props} />);
+
+  expect(btn.textContent).toBe('deviceA1');
+  expect(props.selectedDevice).toBe(deviceA1);
+
+});
+
+it("simulate clicks", async () => {
+  const deviceA1 = new MediaDevice('a1', 'audioinput', 'deviceA1');
+  const props: MediaDeviceSelectProps = {
+    id: 'an-id',
+    testid: 'a-test-id',
+    devices: {
+      [deviceA1.getId()]: deviceA1
+    },
+    selectedDevice: undefined,
+    setSelectedDevice: jest.fn()
+  };
+  const { rerender } = render(<MediaDeviceSelect {...props} />);
+
+  const select = screen.getByTestId('a-test-id');
+
+  expect(select.textContent).toContain('Default');
+
+  // click on select
+  UserEvent.click(screen.getByRole("button"));
+
+  // click to select device a1
+  const dropdownItem = await screen.findByRole("option", { name: deviceA1.getLabel() });
+  act(() => {
+    UserEvent.click(dropdownItem)
+  })
+
+  await waitFor(() => {
+    expect(props.setSelectedDevice).toHaveBeenCalledWith(deviceA1);
+  })
 });
--- src/components/MediaDeviceSelect/MediaDeviceSelect.tsx
+++ src/components/MediaDeviceSelect/MediaDeviceSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import MenuItem from '@mui/material/MenuItem'
 import Select, { SelectChangeEvent, SelectProps } from '@mui/material/Select'
@@ -6,6 +6,7 @@ import Select, { SelectChangeEvent, SelectProps } from '@mui/material/Select'
 import { MediaDevice } from '@apirtc/apirtc'
 
 export interface MediaDeviceSelectProps extends Omit<SelectProps, 'value' | 'onChange'> {
+    testid?: string,
     devices: {
         [key: string]: MediaDevice
     },
@@ -13,10 +14,14 @@ export interface MediaDeviceSelectProps extends Omit<SelectProps, 'value' | 'onC
     setSelectedDevice: Function
 }
 
+const DEFAULT_ID = 'default';
+const DEFAULT_LABEL = 'Default';
+
 const COMPONENT_NAME = "MediaDeviceSelect";
 export function MediaDeviceSelect(props: MediaDeviceSelectProps) {
 
     const { id = 'media-device-selector',
+        testid = 'test-media-device-selector',
         devices,
         selectedDevice,
         setSelectedDevice,
@@ -25,19 +30,22 @@ export function MediaDeviceSelect(props: MediaDeviceSelectProps) {
     const onSelectDevice = (event: SelectChangeEvent<any>) => {
         event.preventDefault()
         if (globalThis.apirtcMuiReactLibLogLevel.isDebugEnabled) {
-            console.debug(COMPONENT_NAME + "|onSelectDevice", event.target.value)
+            console.debug(`${COMPONENT_NAME}|onSelectDevice`, event.target.value)
         }
         setSelectedDevice(devices[event.target.value])
     };
 
+    const defaultDevice = useMemo(() => devices[DEFAULT_ID], [devices]);
+
     const _devices = Object.values(devices).map((device: MediaDevice) => {
         return <MenuItem key={device.getId()} value={device.getId()}>{device.getLabel()}</MenuItem>
-    }).concat(<MenuItem key={'default'} value={'default'}>default</MenuItem>);
+    });
 
-    return <Select id={id}
-        value={selectedDevice ? selectedDevice.getId() : 'default'}
+    return <Select id={id} data-testid={testid}
+        value={selectedDevice ? selectedDevice.getId() : DEFAULT_ID}
         onChange={onSelectDevice}
         {...rest}>
+        {!defaultDevice && <MenuItem key={DEFAULT_ID} value={DEFAULT_ID}>{DEFAULT_LABEL}</MenuItem>}
         {_devices}
     </Select>
 }
